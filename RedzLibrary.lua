143b3b499fa468b47a452b2867273551614b10ad
🎩 THE MR RED BLACK OWNER 🎩 <miguelgoncalvesgoes@gmail.com>
2026-03-30 17:49:13 -0300

    Update RedzLibrary.lua

--- RedzLibrary.lua
+++ RedzLibrary.lua
@@ -41,7 +41,7 @@ local redzlib = {
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))
 			}),
 			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-			["Color Stroke"] = Color3.fromRGB(255, 0, 0),
+			["Color Stroke"] = Color3.fromRGB(1, 0, 0),
 			["Color Theme"] = Color3.fromRGB(0, 0, 0),
 			["Color Text"] = Color3.fromRGB(255, 255, 255),
 			["Color Dark Text"] = Color3.fromRGB(200, 200, 200)
