5e620349161981e22dbd4993c750e909f67d1f41
🎩 THE MR RED BLACK OWNER 🎩 <miguelgoncalvesgoes@gmail.com>
2026-03-30 17:52:00 -0300

    Change color values for text and stroke
    Updated color values for 'Color Text' and 'Color Stroke' in both light and dark themes.

--- RedzLibrary.lua
+++ RedzLibrary.lua
@@ -19,7 +19,7 @@ local redzlib = {
 			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
 			["Color Stroke"] = Color3.fromRGB(255, 0, 0),
 			["Color Theme"] = Color3.fromRGB(1, 0, 0),
-			["Color Text"] = Color3.fromRGB(255, 255, 255),
+			["Color Text"] = Color3.fromRGB(1, 0, 0),
 			["Color Dark Text"] = Color3.fromRGB(200, 200, 200)
 		},
 		Dark = {
@@ -29,7 +29,7 @@ local redzlib = {
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))
 			}),
 			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-			["Color Stroke"] = Color3.fromRGB(255, 0, 0),
+			["Color Stroke"] = Color3.fromRGB(1, 0, 0),
 			["Color Theme"] = Color3.fromRGB(255, 0, 0),
 			["Color Text"] = Color3.fromRGB(255, 255, 255),
 			["Color Dark Text"] = Color3.fromRGB(200, 200, 200)
